2b8020c742bcc3e70f4022a4f9792c36cc99c1ad
ThomasJejkal <thomas.jejkal@kit.edu>
2019-10-14 11:39:19 +0200

    Enabled build for window Finished workflow for schema registry and metadata repository controller Changes to model in order to match workflow

--- src/main/java/edu/kit/datamanager/metastore2/domain/MetadataSchemaRecord.java
+++ src/main/java/edu/kit/datamanager/metastore2/domain/MetadataSchemaRecord.java
@@ -19,16 +19,19 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import edu.kit.datamanager.entities.repo.AclEntry;
 import edu.kit.datamanager.util.json.CustomInstantDeserializer;
 import edu.kit.datamanager.util.json.CustomInstantSerializer;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.Set;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import javax.persistence.IdClass;
 import lombok.Data;
 
 /**
@@ -39,32 +42,39 @@ import lombok.Data;
 @JsonIgnoreProperties(ignoreUnknown = true)
 @ApiModel(description = "Metadata schema record")
 @Data
-public class MetadataSchemaRecord implements Serializable{
+@IdClass(MetadataSchemaId.class)
+public class MetadataSchemaRecord implements Serializable {
 
-  public enum SCHEMA_TYPE{
-    JSON,
-    XML
-    //,PROPERTIES
-  }
-  @Id
-  @ApiModelProperty(value = "The unqiue identifier of the schema used in the metadata repository for identifying the schema.", dataType = "String", example = "dc1.1")
-  private String schemaId;
-  @ApiModelProperty(value = "The mime type this schema is associated with. The mime type should be as specific as possible in order to allow an easy mapping of documents matching the schema.", dataType = "String", example = "application/vnd.datamanager.data-resource+json")
-  private String mimeType;
-  @Enumerated(EnumType.STRING)
-  @ApiModelProperty(value = "The schema type used for quick decision making, e.g. to select a proper validator.", example = "JSON")
-  private SCHEMA_TYPE type;
-  @ApiModelProperty(value = "The date the record has been initially created.", example = "2017-05-10T10:41:00Z", required = true)
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-  @JsonDeserialize(using = CustomInstantDeserializer.class)
-  @JsonSerialize(using = CustomInstantSerializer.class)
-  private Instant createdAt;
-  @ApiModelProperty(value = "The date the record had been updated the last time.", example = "2017-05-10T10:41:00Z", required = true)
-  @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
-  @JsonDeserialize(using = CustomInstantDeserializer.class)
-  @JsonSerialize(using = CustomInstantSerializer.class)
-  private Instant lastUpdate;
-  @ApiModelProperty(value = "The schema document uri, e.g. pointing to a local file.")
-  private String schemaDocumentUri;
+    public enum SCHEMA_TYPE {
+        JSON,
+        XML
+        //,PROPERTIES
+    }
+    @Id
+    @ApiModelProperty(value = "The unqiue identifier of the schema used in the metadata repository for identifying the schema.", dataType = "String", example = "dc")
+    private String schemaId;
+    @Id
+    @ApiModelProperty(value = "The schema version.", dataType = "Integer")
+    private Integer schemaVersion;
+
+    @ApiModelProperty(value = "The mime type this schema is associated with. The mime type should be as specific as possible in order to allow an easy mapping of documents matching the schema.", dataType = "String", example = "application/vnd.datamanager.data-resource+json")
+    private String mimeType;
+    @Enumerated(EnumType.STRING)
+    @ApiModelProperty(value = "The schema type used for quick decision making, e.g. to select a proper validator.", example = "JSON")
+    private SCHEMA_TYPE type;
+    @ApiModelProperty(value = "The date the record has been initially created.", example = "2017-05-10T10:41:00Z", required = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+    @JsonDeserialize(using = CustomInstantDeserializer.class)
+    @JsonSerialize(using = CustomInstantSerializer.class)
+    private Instant createdAt;
+    @ApiModelProperty(value = "The date the record had been updated the last time.", example = "2017-05-10T10:41:00Z", required = true)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "UTC")
+    @JsonDeserialize(using = CustomInstantDeserializer.class)
+    @JsonSerialize(using = CustomInstantSerializer.class)
+    private Instant lastUpdate;
+    @ApiModelProperty(value = "A list of access control entries for resticting access.")
+    private Set<AclEntry> acl;
+    @ApiModelProperty(value = "The schema document uri, e.g. pointing to a local file.")
+    private String schemaDocumentUri;
 
 }
--- src/main/java/edu/kit/datamanager/metastore2/web/impl/MetadataControllerImpl.java
+++ src/main/java/edu/kit/datamanager/metastore2/web/impl/MetadataControllerImpl.java
@@ -39,66 +39,111 @@ import org.springframework.web.util.UriComponentsBuilder;
 @Controller
 @RequestMapping(value = "/api/v1/metadata")
 @Api(value = "Metadata Repository")
-public class MetadataControllerImpl implements IMetadataController{
+public class MetadataControllerImpl implements IMetadataController {
 
-  @Override
-  public ResponseEntity createRecord(
-          @RequestPart(name = "record") final MetadataRecord record,
-          @RequestPart(name = "document") final MultipartFile document,
-          WebRequest request,
-          HttpServletResponse response,
-          UriComponentsBuilder uriBuilder){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public ResponseEntity createRecord(
+            @RequestPart(name = "record") final MetadataRecord record,
+            @RequestPart(name = "document") final MultipartFile document,
+            WebRequest request,
+            HttpServletResponse response,
+            UriComponentsBuilder uriBuilder) {
+        //check required attributes (relatedResource, schemaId, [schemaVersion]) -> if not set return HTTP BAD_REQUEST
+        //obtain schemaDocument for schemaid and version -> if not found HTTP NOT_FOUND
+        //validate document using schema -> if fails, return HTTP BAD_REQUEST
+        //persist document
+        //update metadataDocumentUri in record 
+        //set createdAt and lastUpdate to Instant.now()
+        //check acl -> add at least caller to acl
 
-  @Override
-  public ResponseEntity<MetadataRecord> getRecordById(
-          @PathVariable(value = "id") String id,
-          @RequestParam(value = "version", required = false) Long version,
-          WebRequest wr,
-          HttpServletResponse hsr){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+        //persist record
+        //update metadataDocumentUri to path of getMetadataDocumentById(id, version)
+        //return HTTP 201 with record
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-  @Override
-  public ResponseEntity getMetadataDocumentById(
-          String id,
-          Long version,
-          WebRequest wr,
-          HttpServletResponse hsr){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public ResponseEntity<MetadataRecord> getRecordById(
+            @PathVariable(value = "id") String id,
+            @RequestParam(value = "version", required = false) Long version,
+            WebRequest wr,
+            HttpServletResponse hsr) {
+        //obtain record by id and version -> if not found, return HTTP NOT_FOUND
+        //if security enabled, check permission -> if not matching, return HTTP UNAUTHORIZED or FORBIDDEN
+        //update metadataDocumentUri to path of getMetadataDocumentById(id, version) 
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-  @Override
-  public ResponseEntity<List<MetadataRecord>> getRecords(
-          @RequestParam(value = "resoureId", required = false) List<String> relatedIds,
-          @RequestParam(value = "schemaId", required = false) List<String> schemaIds,
-          @RequestParam(name = "from", required = false) Instant updateFrom,
-          @RequestParam(name = "until", required = false) Instant updateUntil,
-          Pageable pgbl,
-          WebRequest wr,
-          HttpServletResponse hsr,
-          UriComponentsBuilder ucb){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public ResponseEntity getMetadataDocumentById(
+            String id,
+            Long version,
+            WebRequest wr,
+            HttpServletResponse hsr) {
+        //obtain record by id and version -> if not found, return HTTP NOT_FOUND
+        //if security enabled, check permission -> if not matching, return HTTP UNAUTHORIZED or FORBIDDEN
+        //obtain metadataDocumentUri from record -> if file not found return HTTP NOT_FOUND
+        //return HTTP 200 with streamed file
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-  @Override
-  public ResponseEntity<MetadataRecord> updateRecord(
-          @PathVariable("id") String id,
-          @RequestPart(name = "record", required = false) final MetadataRecord record,
-          @RequestPart(name = "document", required = false) final MultipartFile document,
-          WebRequest request,
-          HttpServletResponse response,
-          UriComponentsBuilder uriBuilder){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public ResponseEntity<List<MetadataRecord>> getRecords(
+            @RequestParam(value = "resoureId", required = false) List<String> relatedIds,
+            @RequestParam(value = "schemaId", required = false) List<String> schemaIds,
+            @RequestParam(name = "from", required = false) Instant updateFrom,
+            @RequestParam(name = "until", required = false) Instant updateUntil,
+            Pageable pgbl,
+            WebRequest wr,
+            HttpServletResponse hsr,
+            UriComponentsBuilder ucb) {
+        //search for records with provided relatedIds AND schemaIds including updateFrom and updateUntil for lastUpdate field with the biggest version number
+        //if security is enabled, include principal in query
+        //set all metadataDocumentUri elemets to path of getMetadataDocumentById(schemaId, version)
+        //return HTTP 200 and result list
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
-  @Override
-  public ResponseEntity deleteRecord(
-          @PathVariable(value = "id") String id,
-          WebRequest wr,
-          HttpServletResponse hsr){
-    throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-  }
+    @Override
+    public ResponseEntity<MetadataRecord> updateRecord(
+            @PathVariable("id") String id,
+            @RequestPart(name = "record", required = false) final MetadataRecord record,
+            @RequestPart(name = "document", required = false) final MultipartFile document,
+            WebRequest request,
+            HttpServletResponse response,
+            UriComponentsBuilder uriBuilder) {
+        //search for record with provided id in the most recent version -> if not found, return HTTP NOT_FOUND
+        //if authorization enabled, check principal -> return HTTP UNAUTHORIZED or FORBIDDEN if not matching
+        //ETag check -> HTTP CONFLICT if fails
+        //check for valid fields to update relatedResource, schemaId, schemaVersion, acl) -> return HTTP BAD_REQUEST if forbidden fields are included
+        //apply schemaId and schemaVersion update
+
+        //if document is provided:
+        //compare current document with provided and check for difference -> no difference, proceed at end
+        //obtain schema for schemaId and version -> if fails return HTTP NOT_FOUND
+        //validate document using schema -> if fails, return HTTP BAD_REQUEST
+        //persist document
+        //end if document provided
+        //apply changes to record (relatedResource, acl)
+        //update record version
+        //update schemaDocumentUri (if document was provided)
+        //update lastModifiedAt to Instant.now()
+        //Persist record
+        //set metadataDocumentUri to path of getMetadataDocumentById(id, version)
+        //return HTTP 200 and record
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ResponseEntity deleteRecord(
+            @PathVariable(value = "id") String id,
+            WebRequest wr,
+            HttpServletResponse hsr) {
+        //search for all versions of record with provided id
+        //ETag check with most recent version -> HTTP CONFLICT if fails
+        //Delete documents and records
+        //return HTTP 204
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
