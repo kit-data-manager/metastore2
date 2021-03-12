2edb222028c525a2c1a8087ca5e20bd522e72081
Volker Hartmann <volker.hartmann@kit.edu>
2021-03-12 12:09:41 +0100

    Add queries for date range.

--- src/main/java/edu/kit/datamanager/metastore2/dao/IDataRecordDao.java
+++ src/main/java/edu/kit/datamanager/metastore2/dao/IDataRecordDao.java
@@ -6,6 +6,7 @@
 package edu.kit.datamanager.metastore2.dao;
 
 import edu.kit.datamanager.metastore2.domain.DataRecord;
+import java.time.Instant;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -17,4 +18,7 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 public interface IDataRecordDao extends JpaRepository<DataRecord, String>, JpaSpecificationExecutor<DataRecord>{
   DataRecord findByMetadataId(String metadataId);
   List<DataRecord> findBySchemaId(String schemaId);
+  List<DataRecord> findBySchemaIdAndLastUpdateAfter(String schemaId, Instant from);
+  List<DataRecord> findBySchemaIdAndLastUpdateBefore(String schemaId, Instant until);
+  List<DataRecord> findBySchemaIdAndLastUpdateAfterAndLastUpdateBefore(String schemaId, Instant from, Instant until);
 }
