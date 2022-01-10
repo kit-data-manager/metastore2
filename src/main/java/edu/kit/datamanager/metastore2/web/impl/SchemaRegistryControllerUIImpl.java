bfb9fe0b1587d8dc0a37a55620f47837596a3167
Sabrine Chelbi <sabrine.chelbi@kit.edu>
2022-01-10 16:28:15 +0100

    Fixed #64 - using InputStream for reading resources

--- src/main/java/edu/kit/datamanager/metastore2/web/impl/SchemaRegistryControllerUIImpl.java
+++ src/main/java/edu/kit/datamanager/metastore2/web/impl/SchemaRegistryControllerUIImpl.java
@@ -201,7 +201,7 @@ public class SchemaRegistryControllerUIImpl implements ISchemaRegistryController
         TabulatorItems[] items = null;
         Resource resource = new ClassPathResource(path);
         try {
-            items = mapper.readValue(Files.newBufferedReader(Paths.get(resource.getURI()), StandardCharsets.UTF_8), TabulatorItems[].class);
+            items = mapper.readValue(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8), TabulatorItems[].class);
         } catch (IOException ex) {
             Logger.getLogger(SchemaRegistryControllerUIImpl.class.getName()).log(Level.SEVERE, null, ex);
         }
