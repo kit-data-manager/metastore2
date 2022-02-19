c085c75f2eee05ff0cecb52d10341621a7777df0
Sabrine Chelbi <sabrine.chelbi@kit.edu>
2022-02-19 19:41:53 +0100

    Refactoring

--- src/main/java/edu/kit/datamanager/metastore2/web/ISchemaRegistryControllerUI.java
+++ src/main/java/edu/kit/datamanager/metastore2/web/ISchemaRegistryControllerUI.java
@@ -5,8 +5,11 @@
  */
 package edu.kit.datamanager.metastore2.web;
 
+import edu.kit.datamanager.metastore2.dto.TabulatorRemotePagination;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -16,18 +19,18 @@ import org.springframework.web.util.UriComponentsBuilder;
  * @author sabrinechelbi
  */
 public interface ISchemaRegistryControllerUI {
+    
+    public ResponseEntity<TabulatorRemotePagination> getSchemaRecordsForUi(Pageable pgbl, WebRequest wr, HttpServletResponse hsr, UriComponentsBuilder ucb);
+    
+    public ResponseEntity<TabulatorRemotePagination> getMetadataRecordsForUi(@RequestParam(value = "id", required = false) String id, Pageable pgbl, WebRequest wr, HttpServletResponse hsr, UriComponentsBuilder ucb);
 
     public ModelAndView schemaManagement();
 
-    public ModelAndView metadataSchemaManagement(String id, Pageable pgbl,
+    public ModelAndView metadataManagement(Pageable pgbl,
             WebRequest wr,
             HttpServletResponse hsr,
             UriComponentsBuilder ucb);
     
-    public ModelAndView metadataManagement();
-
     public String dashboard();
-
-    public String startPage();
     
 }
