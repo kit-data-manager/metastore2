a5bcb166990c37c5ee247c9fc947ad592b666d57
Sabrine Chelbi <sabrine.chelbi@kit.edu>
2022-02-25 09:19:15 +0100

    Added Feature in the Metadata Editor - datetime formatter

---
+++ src/main/java/edu/kit/datamanager/metastore2/dto/TabulatorFormatterParam.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kit.datamanager.metastore2.dto;
+
+import lombok.Getter;
+
+/**
+ *
+ * @author sabrinechelbi
+ */
+@Getter
+public class TabulatorFormatterParam {
+    private String outputFormat;
+}
--- src/main/resources/static/editor/dependencies/jsonform/lib/jsonform.js
+++ src/main/resources/static/editor/dependencies/jsonform/lib/jsonform.js
@@ -268,7 +268,24 @@
             'inputfield': true
         }
     };
-
+    
+    var dateTimeFieldTemplate = function (type) {
+        return {
+            'template': '<input type="' + type + '" ' +
+                    'class=\'form-control<%= (fieldHtmlClass ? " " + fieldHtmlClass : "") %>\'' +
+                    'name="<%= node.name %>" value="<%= escape(value.slice(0, 16).replace("T"," ")) %>" id="<%= id %>"' +
+                    '<%= (node.disabled? " disabled" : "")%>' +
+                    '<%= (node.readOnly ? " readonly=\'readonly\'" : "") %>' +
+                    '<%= (node.schemaElement && (node.schemaElement.step > 0 || node.schemaElement.step == "any") ? " step=\'" + node.schemaElement.step + "\'" : "") %>' +
+                    '<%= (node.schemaElement && node.schemaElement.maxLength ? " maxlength=\'" + node.schemaElement.maxLength + "\'" : "") %>' +
+                    '<%= (node.schemaElement && node.schemaElement.required && (node.schemaElement.type !== "boolean") ? " required=\'required\'" : "") %>' +
+                    '<%= (node.placeholder? " placeholder=" + \'"\' + escape(node.placeholder) + \'"\' : "")%>' +
+                    ' />',
+            'fieldtemplate': true,
+            'inputfield': true
+        }
+    };
+    
     jsonform.elementTypes = {
         'none': {
             'template': ''
@@ -279,7 +296,7 @@
         'text': inputFieldTemplate('text'),
         'password': inputFieldTemplate('password'),
         'date': inputFieldTemplate('date'),
-        'datetime': inputFieldTemplate('datetime'),
+        'datetime': dateTimeFieldTemplate('datetime'),
         'datetime-local': inputFieldTemplate('datetime-local'),
         'email': inputFieldTemplate('email'),
         'month': inputFieldTemplate('month'),
@@ -1450,8 +1467,7 @@
             }
         }
     };
-
-
+    
 //Allow to access subproperties by splitting "."
     /**
      * Retrieves the key identified by a path selector in the structured object.
