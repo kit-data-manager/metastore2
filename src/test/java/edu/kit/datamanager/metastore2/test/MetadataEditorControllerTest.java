b5421ccc59ae0ecf4b6b9569f10ac3d16484ccba
Volker Hartmann <volker.hartmann@kit.edu>
2023-06-26 14:36:45 +0200

    Add test for UI.

---
+++ src/test/java/edu/kit/datamanager/metastore2/test/MetadataEditorControllerTest.java
@@ -0,0 +1,106 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.kit.datamanager.metastore2.test;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.security.web.FilterChainProxy;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+/**
+ *
+ * @author Torridity
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) //RANDOM_PORT)
+@EntityScan("edu.kit.datamanager")
+@EnableJpaRepositories("edu.kit.datamanager")
+@ComponentScan({"edu.kit.datamanager"})
+@AutoConfigureMockMvc
+@TestExecutionListeners(listeners = {ServletTestExecutionListener.class,
+  DependencyInjectionTestExecutionListener.class,
+  DirtiesContextTestExecutionListener.class,
+  TransactionalTestExecutionListener.class,
+  WithSecurityContextTestExecutionListener.class})
+@ActiveProfiles("test")
+@TestPropertySource(properties = {"server.port=41419"})
+@TestPropertySource(properties = {"metastore.schema.schemaFolder=file:///tmp/metastore2/editor/schema"})
+@TestPropertySource(properties = {"metastore.metadata.metadataFolder=file:///tmp/metastore2/editor/metadata"})
+@TestPropertySource(properties = {"spring.datasource.url=jdbc:h2:mem:db_md_editor;DB_CLOSE_DELAY=-1;MODE=LEGACY;NON_KEYWORDS=VALUE"})
+@TestPropertySource(properties = {"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"})
+@TestPropertySource(properties = {"spring.jpa.defer-datasource-initialization=true"})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class MetadataEditorControllerTest {
+
+  private MockMvc mockMvc;
+  @Autowired
+  private WebApplicationContext context;
+  @Autowired
+  private FilterChainProxy springSecurityFilterChain;
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  @Before
+  public void setUp() throws Exception {
+    System.out.println("------MetadataEditorControllerTest--------------------------");
+
+
+      // setup mockMvc
+      this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+              .addFilters(springSecurityFilterChain)
+              .apply(documentationConfiguration(this.restDocumentation).uris()
+                      .withPort(41419))
+              .build();
+  }
+
+  @Test
+  public void testGetDashboard() throws Exception {
+    MvcResult res = this.mockMvc.perform(get("/dashboard"))
+            .andDo(print()).andExpect(status().isOk()).andReturn();
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Dashboard"));
+  }
+
+  @Test
+  public void testGetSchemaManagement() throws Exception {
+    MvcResult res = this.mockMvc.perform(get("/schema-management"))
+            .andDo(print()).andExpect(status().isOk()).andReturn();
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Schema Management"));
+  }
+
+  @Test
+  public void testGetMetadataManagement() throws Exception {
+    MvcResult res = this.mockMvc.perform(get("/metadata-management"))
+            .andDo(print()).andExpect(status().isOk()).andReturn();
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Metadata Management"));
+  }
+}
