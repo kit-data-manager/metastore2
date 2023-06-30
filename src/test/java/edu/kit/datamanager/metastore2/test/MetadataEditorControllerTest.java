d35a73660fe3e1a64d012fb3556588e2b048a2dd
Volker Hartmann <volker.hartmann@kit.edu>
2023-06-30 08:22:59 +0200

    Add tests for UI (to check accessibility).

--- src/test/java/edu/kit/datamanager/metastore2/test/MetadataEditorControllerTest.java
+++ src/test/java/edu/kit/datamanager/metastore2/test/MetadataEditorControllerTest.java
@@ -5,35 +5,6 @@
  */
 package edu.kit.datamanager.metastore2.test;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.type.CollectionType;
-import edu.kit.datamanager.entities.PERMISSION;
-import edu.kit.datamanager.metastore2.configuration.MetastoreConfiguration;
-import edu.kit.datamanager.metastore2.dao.IDataRecordDao;
-import edu.kit.datamanager.metastore2.dao.ILinkedMetadataRecordDao;
-import edu.kit.datamanager.metastore2.dao.ISchemaRecordDao;
-import edu.kit.datamanager.metastore2.domain.MetadataRecord;
-import edu.kit.datamanager.metastore2.domain.MetadataSchemaRecord;
-import edu.kit.datamanager.metastore2.domain.ResourceIdentifier;
-import edu.kit.datamanager.metastore2.domain.ResourceIdentifier.IdentifierType;
-import edu.kit.datamanager.repo.dao.IAllIdentifiersDao;
-import edu.kit.datamanager.repo.dao.IContentInformationDao;
-import edu.kit.datamanager.repo.dao.IDataResourceDao;
-import edu.kit.datamanager.repo.domain.acl.AclEntry;
-import java.io.File;
-import java.io.IOException;
-import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.time.Instant;
-import java.util.Comparator;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Stream;
-import org.hamcrest.Matchers;
-import org.javers.core.Javers;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Rule;
@@ -45,9 +16,6 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.http.HttpHeaders;
-import org.springframework.mock.web.MockHttpServletRequest;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.restdocs.JUnitRestDocumentation;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
@@ -63,17 +31,11 @@ import org.springframework.test.context.transaction.TransactionalTestExecutionLi
 import org.springframework.test.context.web.ServletTestExecutionListener;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import org.springframework.test.web.servlet.request.RequestPostProcessor;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static edu.kit.datamanager.metastore2.test.CreateSchemaUtil.*;
 
 /**
  *
@@ -92,6 +54,8 @@ import static edu.kit.datamanager.metastore2.test.CreateSchemaUtil.*;
   WithSecurityContextTestExecutionListener.class})
 @ActiveProfiles("test")
 @TestPropertySource(properties = {"server.port=41419"})
+@TestPropertySource(properties = {"metastore.schema.schemaFolder=file:///tmp/metastore2/editor/schema"})
+@TestPropertySource(properties = {"metastore.metadata.metadataFolder=file:///tmp/metastore2/editor/metadata"})
 @TestPropertySource(properties = {"spring.datasource.url=jdbc:h2:mem:db_md_editor;DB_CLOSE_DELAY=-1;MODE=LEGACY;NON_KEYWORDS=VALUE"})
 @TestPropertySource(properties = {"spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"})
 @TestPropertySource(properties = {"spring.jpa.defer-datasource-initialization=true"})
@@ -103,55 +67,40 @@ public class MetadataEditorControllerTest {
   private WebApplicationContext context;
   @Autowired
   private FilterChainProxy springSecurityFilterChain;
-  @Autowired
-  Javers javers = null;
-  @Autowired
-  private ILinkedMetadataRecordDao metadataRecordDao;
-  @Autowired
-  private IDataResourceDao dataResourceDao;
-  @Autowired
-  private IDataRecordDao dataRecordDao;
-  @Autowired
-  private ISchemaRecordDao schemaRecordDao;
-  @Autowired
-  private IContentInformationDao contentInformationDao;
-  @Autowired
-  private IAllIdentifiersDao allIdentifiersDao;
-  @Autowired
-  private MetastoreConfiguration metadataConfig;
-  @Autowired
-  private MetastoreConfiguration schemaConfig;
   @Rule
   public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
 
   @Before
   public void setUp() throws Exception {
-    System.out.println("------MetadataControllerTest--------------------------");
-    System.out.println("------" + this.metadataConfig);
-    System.out.println("------------------------------------------------------");
+    System.out.println("------MetadataEditorControllerTest--------------------------");
 
-    contentInformationDao.deleteAll();
-    dataResourceDao.deleteAll();
-    metadataRecordDao.deleteAll();
-    schemaRecordDao.deleteAll();
-    dataRecordDao.deleteAll();
-    allIdentifiersDao.deleteAll();
 
-    // setup mockMvc
-    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
-//            .addFilters(springSecurityFilterChain)
-//            .apply(documentationConfiguration(this.restDocumentation).uris()
-//                    .withPort(41419))
-            .build();
+      // setup mockMvc
+      this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+              .addFilters(springSecurityFilterChain)
+              .apply(documentationConfiguration(this.restDocumentation).uris()
+                      .withPort(41419))
+              .build();
   }
 
   @Test
   public void testGetDashboard() throws Exception {
-
     MvcResult res = this.mockMvc.perform(get("/dashboard"))
             .andDo(print()).andExpect(status().isOk()).andReturn();
-    System.out.println("");
-    Assert.assertTrue(res.getResponse().getContentAsString().contains("Metadata Management"));
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Dashboard"));
+  }
 
+  @Test
+  public void testGetSchemaManagement() throws Exception {
+    MvcResult res = this.mockMvc.perform(get("/schema-management"))
+            .andDo(print()).andExpect(status().isOk()).andReturn();
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Schema Management"));
+  }
+
+  @Test
+  public void testGetMetadataManagement() throws Exception {
+    MvcResult res = this.mockMvc.perform(get("/metadata-management"))
+            .andDo(print()).andExpect(status().isOk()).andReturn();
+    Assert.assertTrue(res.getResponse().getContentAsString().contains("Metadata Management"));
   }
 }
