16f5050ba041f4ac3a36d988bf8a94318d7ddaaf
Volker Hartmann <volker.hartmann@kit.edu>
2024-11-13 16:07:58 +0100

    Remove unused code.

---
+++ src/test/java/edu/kit/datamanager/metastore2/runner/Migrate2DataciteTest.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2023 Karlsruhe Institute of Technology.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.kit.datamanager.metastore2.runner;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.kit.datamanager.entities.PERMISSION;
+import edu.kit.datamanager.metastore2.configuration.MetastoreConfiguration;
+import edu.kit.datamanager.metastore2.dao.IDataRecordDao;
+import edu.kit.datamanager.metastore2.dao.ILinkedMetadataRecordDao;
+import edu.kit.datamanager.metastore2.dao.ISchemaRecordDao;
+import edu.kit.datamanager.metastore2.dao.IUrl2PathDao;
+import edu.kit.datamanager.metastore2.domain.MetadataRecord;
+import edu.kit.datamanager.metastore2.domain.MetadataSchemaRecord;
+import edu.kit.datamanager.metastore2.domain.ResourceIdentifier;
+import edu.kit.datamanager.repo.dao.IAllIdentifiersDao;
+import edu.kit.datamanager.repo.dao.IContentInformationDao;
+import edu.kit.datamanager.repo.dao.IDataResourceDao;
+import edu.kit.datamanager.repo.domain.acl.AclEntry;
+import edu.kit.datamanager.util.AuthenticationHelper;
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Stream;
+import org.javers.core.Javers;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
+import org.springframework.security.test.context.support.WithSecurityContextTestExecutionListener;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.test.context.web.ServletTestExecutionListener;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import static com.github.stefanbirkner.systemlambda.SystemLambda.*;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+/**
+ *
+ * @author hartmann-v
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// Or create a test version of Application.class that stubs out services used by the CommandLineRunner
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT) //RANDOM_PORT)
+@EntityScan("edu.kit.datamanager")
+@EnableJpaRepositories("edu.kit.datamanager")
+@ComponentScan({"edu.kit.datamanager"})
+@AutoConfigureMockMvc
+@TestExecutionListeners(listeners = {ServletTestExecutionListener.class,
+  DependencyInjectionTestExecutionListener.class,
+  DirtiesContextTestExecutionListener.class,
+  TransactionalTestExecutionListener.class,
+  WithSecurityContextTestExecutionListener.class})
+@ActiveProfiles("test")
+@TestPropertySource(properties = {"server.port=41417"})
+@TestPropertySource(properties = {"spring.datasource.url=jdbc:h2:file:./src/test/resources/migrateToV2/migrationDatabase;DB_CLOSE_DELAY=-1;MODE=LEGACY;NON_KEYWORDS=VALUE"})
+@TestPropertySource(properties = {"metastore.schema.schemaFolder=file:///tmp/metastore2/migrationRunner/schema"})
+@TestPropertySource(properties = {"metastore.metadata.metadataFolder=file:///tmp/metastore2/migrationRunner/metadata"})
+@TestPropertySource(properties = {"metastore.metadata.schemaRegistries="})
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public class Migrate2DataciteTest {
+
+  private static Boolean alreadyInitialized = Boolean.FALSE;
+
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ElasticIndexerRunner eir;
+  @Autowired
+  private WebApplicationContext context;
+  @Autowired
+  Javers javers = null;
+  @Autowired
+  private ILinkedMetadataRecordDao metadataRecordDao;
+  @Autowired
+  private IDataResourceDao dataResourceDao;
+  @Autowired
+  private IDataRecordDao dataRecordDao;
+  @Autowired
+  private ISchemaRecordDao schemaRecordDao;
+  @Autowired
+  private IContentInformationDao contentInformationDao;
+  @Autowired
+  private IAllIdentifiersDao allIdentifiersDao;
+  @Autowired
+  private IUrl2PathDao url2PathDao;
+  @Autowired
+  private MetastoreConfiguration metadataConfig;
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
+  public Migrate2DataciteTest() {
+  }
+
+  @BeforeClass
+  public static void setUpClass() {
+  }
+
+  @AfterClass
+  public static void tearDownClass() {
+  }
+
+  @Before
+  public void setUp() {
+    // setup mockMvc
+    this.mockMvc = MockMvcBuilders.webAppContextSetup(this.context)
+            .apply(springSecurity())
+            .apply(documentationConfiguration(this.restDocumentation).uris()
+                    .withPort(41417))
+            .build();
+    eir.indices = null;
+    eir.updateDate = null;
+    eir.updateIndex = false;
+    if (!isInitialized()) {
+      System.out.println("------Migrate2DataciteTest--------");
+      System.out.println("------" + this.metadataConfig);
+      System.out.println("------------------------------------------------------");
+
+    }
+  }
+
+  @After
+  public void tearDown() {
+  }
+
+
+  @Test
+  public void testElasticRunnerMigration() throws Exception {
+    eir.run("--migrate2DataCite");
+    Assert.assertTrue(true);
+  }
+
+  public static synchronized boolean isInitialized() {
+    boolean returnValue = alreadyInitialized;
+    alreadyInitialized = Boolean.TRUE;
+
+    return returnValue;
+  }
+
+}
